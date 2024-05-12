e3f262e0fd1c8e66a0f392f53896e2f8aa472a8c
Najeong Kim <najung39@gmail.com>
2024-05-13 01:51:40 +0900

    8-bit floating point converter

--- 8_bits_floating_point_ALU/main.c
+++ 8_bits_floating_point_ALU/main.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define EXP_BITS 3
 #define BIAS 3
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,17 +15,16 @@ int check_right_scan(int scan_items, char op) {
 }
 
 void FP8(float fnum, char* object, char* FP8_fnum) {
-	/*STEP1 : decimal to binary 2.5를 2진법으로 바꾸면 10.1*/
-	/*부호*/
+	/*sign*/
 	char sign = (fnum >= 0) ? '0' : '1';
 	fnum = (fnum >= 0) ? fnum : -fnum;
-
-	/*정수*/
+	/*integer*/
 	int integer = (int)fnum;
 	char* bi_integer = (char*)malloc(33 * sizeof(char));
 	if (bi_integer == NULL) exit(1);
 
-	if (integer != 0) {
+	if (integer == 0) strcpy(bi_integer, "0");
+	else {
 		int index = 0;
 		while (integer > 0) {
 			bi_integer[index++] = (integer % 2) + '0';
@@ -41,11 +41,8 @@ void FP8(float fnum, char* object, char* FP8_fnum) {
 			start++;
 			end--;
 		}
-
 	}
-	else strcpy(bi_integer, "0");
-
-	/*실수*/
+	/*fraction*/
 	float fraction = fnum - (int)fnum;
 	char* bi_fraction = (char*)malloc(33 * sizeof(char));
 	if (bi_fraction == NULL) {
@@ -59,102 +56,97 @@ void FP8(float fnum, char* object, char* FP8_fnum) {
 			bi_fraction[index++] = '1';
 			fraction -= 1.0;
 		}
-		else {
-			bi_fraction[index++] = '0';
-		}
+		else bi_fraction[index++] = '0';
 	}
-	for (; index < 32; index++) {
-		bi_fraction[index] = '0';
-	}
+	for (; index < 32; index++) bi_fraction[index] = '0';
 	bi_fraction[32] = '\0';
-
 	/*--------------------------------------------*/
-	/*STEP2 : 정규화된 과학적 표기법*/
-
-	/*지수부*/
-	int exponent = 0;
-	float value = fnum;
-	if (value == 0) exponent = -3;
-	while (value >= 2.0) {
-		value /= 2.0;
-		exponent++;
-	}
-	while (value < 1.0 && value != 0.0) {
-		value *= 2.0;
-		exponent--;
-	}
-	
-	/*가수부*/
+	char str_exponent[4];
 	char* mantissa = (char*)malloc(6 * sizeof(char));
 	if (mantissa == NULL) exit(1);
-	
-	char* find_one = (char*)malloc(65 * sizeof(char));
-	if (find_one == NULL) exit(1);
-	sprintf(find_one, "%s%s",bi_integer, bi_fraction);
-
-	char* p_one = strchr(find_one, '1');
-	if (p_one != NULL) {
-		strncpy(mantissa, p_one + 1, 5);
-		mantissa[5] = '\0';
+	/*--------------------------------------------*/
+	float value = fnum;
+	if (value == 0) {
+		strcpy(object, "ZeR");
+		strcpy(str_exponent, "000");
+		strcpy(mantissa, "0000");
 	}
-	else strcpy(mantissa, "00000");
+	else if (value > 0 && value < 0.25) {
+		strcpy(object, "DeN");
+		strcpy(str_exponent, "000");
+		strncpy(mantissa, bi_fraction + 2, 4);
+		mantissa[4] = '\0';
+		//너무 미세한 차이와 범위라서 따로 반올림 안하겠음 해도 정밀도가 1자리라 잘림ㅋ
+	}
+	else if (value >= 0.25 && value < 16) {
+		strcpy(object, "NoR");
+		int exponent = BIAS;
+		while (value >= 2.0) {
+			value /= 2.0;
+			exponent++;
+		}
+		while (value < 1.0 && value != 0.0) {
+			value *= 2.0;
+			exponent--;
+		}
+		char* find_one = (char*)malloc(65 * sizeof(char));
+		if (find_one == NULL) exit(1);
+		sprintf(find_one, "%s%s", bi_integer, bi_fraction);
 
-	/*가수부 반올림*/
-	if (strcmp(mantissa, "11111") != 0 && mantissa[4] == '1') {
-		int carry = 1;
-		for (int i = 3; i >= 0; i--) {
-			if (mantissa[i] == '0' && carry == 1) {
-				mantissa[i] = '1';
-				carry = 0;
-			}
-			else if (mantissa[i] == '1' && carry == 1) {
-				mantissa[i] = '0';
-				carry = 1;
+		char* p_one = strchr(find_one, '1');
+		if (p_one != NULL) {
+			strncpy(mantissa, p_one + 1, 5);
+			mantissa[5] = '\0';
+		}
+		/*가수부 반올림*/
+		if (strcmp(mantissa, "11111") != 0 && mantissa[4] == '1') {
+			int carry = 1;
+			for (int i = 3; i >= 0; i--) {
+				if (mantissa[i] == '0' && carry == 1) {
+					mantissa[i] = '1';
+					carry = 0;
+				}
+				else if (mantissa[i] == '1' && carry == 1) {
+					mantissa[i] = '0';
+					carry = 1;
+				}
 			}
 		}
-	}
-	mantissa[4] = '\0';
-
-	/*specialized number구분*/
-	if (exponent < -3) exponent = -3;
-	if (exponent > 4) exponent = 4;
-	exponent += BIAS;
-
-	char str_exponent[4];
-	if (exponent == 0) {
-		strcpy(str_exponent, "000");
-		if (strcmp(mantissa, "0000") == 0) strcpy(object, "ZeR");
-		else strcpy(object, "DeN");
-	}
-	else if (exponent == 7) {
-		strcpy(str_exponent, "111");
-		if (strcmp(mantissa, "0000") == 0) strcpy(object, "InF");
-		else strcpy(object, "NaN");
+		mantissa[4] = '\0';
+		_itoa(exponent, str_exponent, 2);
+		int fill_zero = EXP_BITS - strlen(str_exponent);
+		if (fill_zero > 0) {
+			//앞부분을 0으로 채우기
+			char temp_str[10];
+			memset(temp_str, '0', fill_zero);
+			temp_str[fill_zero] = '\0';
+			strcat(temp_str, str_exponent);
+			strcpy(str_exponent, temp_str);
+		}
 	}
 	else {
-		strcpy(object, "NoR");
-		_itoa(exponent, str_exponent, 2);
+		strcpy(object, "InF");
+		strcpy(str_exponent, "111");
+		strcpy(mantissa, "0000");
 	}
+
 	sprintf(FP8_fnum, "%c%s%s", sign, str_exponent, mantissa);
 }
 
 
 /*메인 함수*/
 int main(void) {
-	char op;
-	float fnum1, fnum2;
-	int scan_items;
+	float fnum;
 	char object[4];
 	char FP8_fnum[9];
 
-	/*read operator  and 2 float numbers*/
-	printf("read operator '+' or '-' and 2 float numbers :");
-	scan_items = scanf("%c %f %f", &op, &fnum1, &fnum2);
-	if (check_right_scan(scan_items, op) == 1) return 1;
+	/*read float number*/
+	printf("Enter the number you want to express in FP8 : ");
+	scanf("%f", &fnum);
 
 	/*translate floating number to FP8*/
-	FP8(fnum1, &object, &FP8_fnum);
-	printf("유형 : %s, 2진수 표현 : %s", object, FP8_fnum);
+	FP8(fnum, &object, &FP8_fnum);
+	printf("NUM TYPE : %s, translate_FP8 : %s", object, FP8_fnum);
 
 	return 0;
 }
